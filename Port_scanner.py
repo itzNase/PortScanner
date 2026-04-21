836e05f98bb2148e96f9724c62e3f6fe9c7d6df9
nase <149661124+itzNase@users.noreply.github.com>
2026-04-21 03:24:28 +0300

    Port_scanner.py
    Multi-threaded port scanner that scans all 65,535 ports with progress bar, color output and IP validation.

--- Port_scanner.py
+++ Port_scanner.py
@@ -1,33 +1,23 @@
 import subprocess
 import sys
-
-
 def install(package):
     subprocess.call([sys.executable, '-m', 'pip', 'install', package])
-
-
 try:
     import colorama
 except ImportError:
     install('colorama')
     import colorama
-
     colorama.init()
 import threading
 import socket
 from colorama import Fore, Style, init
-
 init(autoreset=True, strip=False, convert=False)
-
-
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
     s.close()
     return ip
-
-
 print(Style.DIM + Fore.RED + (r"""
   __  __           _        ____          _   _                
  |  \/  | __ _  __| | ___  | __ ) _   _  | \ | | __ _ ___  ___ 
@@ -42,12 +32,10 @@ open_ports = []
 invalid_ip = False
 total_ports = 65535
 scanned = [0]
-
 def print_progress(cuurent, total):
     percent = int((cuurent / total) * 100)
     bar = '█' * (percent // 2) + "░" * (50 - percent // 2)
     print(f'\rScaning: |{bar}| {percent}% |', end='', flush=True)
-
 def scan_port(ip, port, Timeout):
     global invalid_ip
     with semaphore:
@@ -65,13 +53,10 @@ def scan_port(ip, port, Timeout):
     with lock:
         scanned[0] += 1
         print_progress(scanned[0], total_ports)
-
-
 try:
     print(Fore.WHITE + (f"Results may vary depending on firewall settings."))
     print(Fore.BLUE + (f"127.0.0.1"), Fore.RED + ("→"), Fore.YELLOW + ("scans your local machine most reliable"))
     print(Fore.BLUE + ("192.168.x.x"), Fore.RED + ("→"), Fore.YELLOW + ("may be blocked by windows firewall  "))
-
     IP = input(Style.DIM + Fore.LIGHTWHITE_EX + (f"Target IP: "))
     try:
         socket.inet_aton(IP)
@@ -88,10 +73,8 @@ try:
         t = threading.Thread(target=scan_port, args=(IP, port, Timeout))
         threads.append(t)
         t.start()
-
     for t in threads:
         t.join()
-
     if invalid_ip:
             print(Fore.RED + (f"Invalid IP address '{IP}'"))
     elif open_ports:
